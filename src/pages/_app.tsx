4ea3be02386f993b0a07cb5548e241afb25386b9
Martin LEBLANCS <martin.leblancs@epitech.eu>
2025-10-20 18:50:27 +0200

    [ADD] Analytics package

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,6 +7,8 @@ import { ThemeProvider } from "next-themes";
 import "../styles/globals.css";
 import "../styles/index.css";
 import ToriiEntrance from "../components/ToriiEntrance";
+import { Analytics } from "@vercel/analytics/next"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   const [showEntrance, setShowEntrance] = useState(true);
@@ -17,6 +19,8 @@ export default function MyApp({ Component, pageProps }: AppProps) {
       messages={pageProps.translations}
     >
       <ThemeProvider attribute="class" defaultTheme="light" enableSystem>
+        <Analytics />
+        <SpeedInsights />
         {showEntrance && pageProps.translations?.entrance && (
           <ToriiEntrance
             onComplete={() => setShowEntrance(false)}

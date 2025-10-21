6eb2fc1131e85692c6e0deb9a23d52d8f4c61369
Martin LEBLANCS <martin.leblancs@epitech.eu>
2025-10-21 18:20:27 +0200

    [FIX] Remove Chinese + new music

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,13 +2,13 @@
 
 import { useState } from "react";
 import type { AppProps } from "next/app";
-import { IntlProvider } from 'next-intl';
+import { IntlProvider } from "next-intl";
 import { ThemeProvider } from "next-themes";
 import "../styles/globals.css";
 import "../styles/index.css";
 import ToriiEntrance from "../components/ToriiEntrance";
-import { Analytics } from "@vercel/analytics/next"
-import { SpeedInsights } from "@vercel/speed-insights/next"
+import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   const [showEntrance, setShowEntrance] = useState(true);
@@ -21,6 +21,7 @@ export default function MyApp({ Component, pageProps }: AppProps) {
       <ThemeProvider attribute="class" defaultTheme="light" enableSystem>
         <Analytics />
         <SpeedInsights />
+
         {showEntrance && pageProps.translations?.entrance && (
           <ToriiEntrance
             onComplete={() => setShowEntrance(false)}
